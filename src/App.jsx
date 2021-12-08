7555ba6bc35ac3ce0ff7e6cfd5f91147ca26f9f0
Alberto <tejotaesi@gmail.com>
2021-12-08 16:01:52 -0300

    Construção da tela de inicio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TelaInicial from "./components/components-tela-inicio/TelaInicial";
+
+
+
+export default function App(){
+    return (
+        <>
+        <TelaInicial />
+        </>
+    );
+}
---
+++ src/components/components-tela-inicio/TelaInicial.jsx
@@ -0,0 +1,14 @@
+export default function TelaInicial(){
+    return (
+        <>
+            <div className="tela-inicial">
+                <img className= "logo" src="./assets/logo.png" />
+                <div className="elemento-botao">
+                   <span>Praticar React</span>
+                   <img src="./assets/next.png" />
+                </div>
+            </div>
+       
+        </>
+        );
+}
