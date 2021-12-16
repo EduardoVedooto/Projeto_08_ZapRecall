f785449cc63aaeb3014522593c5ae0243c788f28
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-12-16 10:24:00 -0300

    Preparando revisão de código

--- projetozaprecall/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logomini from ".././assets/logo-mini.png"
-
-export default function Header() {
-    return (
-        <header>
-            <img src={logomini} />
-        </header>
-    );
-}
